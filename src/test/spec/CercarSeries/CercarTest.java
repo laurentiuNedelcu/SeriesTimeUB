566ace28b75464589ff6f88c634e25e748eda957
joaquimYuste <yuste321@gmail.com>
2018-10-14 19:49:01 +0200

    Commit inicial

---
+++ src/test/spec/CercarSeries/CercarTest.java
@@ -0,0 +1,9 @@
+package test.spec.CercarSeries;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+
+@RunWith(ConcordionRunner.class)
+public class CercarTest {
+}
