c45aa2740efbc1279ae7b7eb977a59cdc0fbe155
joaquimYuste <yuste321@gmail.com>
2018-11-26 00:22:47 +0100

    Test acceptació logejar usuari

--- test/spec/LoguejarUsuari/LoguejarUsuari.java
+++ test/spec/LoguejarUsuari/LoguejarUsuari.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 @RunWith(ConcordionRunner.class)
 public class LoguejarUsuari {
     private Controlador controller;
+    private Usuari user;
 
     @BeforeExample
     public void init(){
@@ -17,5 +18,25 @@ public class LoguejarUsuari {
     }
 
     @Test
-    public Usuari logInUsuari() {return controller.logInUsuari("Nickname","Password");}
+    public boolean logInUsuari() {
+        user = controller.registreUsuari("Campió","Monmteló","Nickname","Password","12/14/97");
+        if(user.equals(controller.logInUsuari("Nickname","Password")))
+            return true;
+        return false;
+    }
+
+    @Test
+    public boolean logInUsuariNoRegistrat(){
+        if(controller.logInUsuari("Nickname","Password")==null)
+            return true;
+        return false;
+    }
+
+    @Test
+    public boolean logInUsuariContrasenyaIncorrecta(){
+        user = controller.registreUsuari("Campió","Monmteló","Nickname","Password","12/14/97");
+        if(user.equals(controller.logInUsuari("Nickname","Passwrdo")))
+            return true;
+        return false;
+    }
 }
