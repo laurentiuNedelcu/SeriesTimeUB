12395706af46c6709747aff9d39be5ebee721c6e
marcosplazagonzalez <marcos.plaza.gonzalez@gmail.com>
2018-11-26 19:28:24 +0100

    Algunes modificacions més
    Falta acabar Diagrama Classes

--- src/model/Administrador.java
+++ src/model/Administrador.java
@@ -1,18 +1,16 @@
 package model;
 
 public class Administrador extends Persona {
-    private int id;
     private String nickname, password;
 
     public Administrador(){
         super();
-        this.id = 0;
         this.nickname = "None";
         this.password = "None";
     }
 
-    public Administrador (int id, String nickname, String password){
-        this.id = id;
+    public Administrador (int id, String nom_real, String nacionalitat, String nickname, String password){
+        super(id,nom_real,nacionalitat);
         this.nickname = nickname;
         this.password = password;
     }
