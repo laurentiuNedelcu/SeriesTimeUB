5370b2f65fa6aa83c8a1a88095a19f7ba56658b8
marcosplazagonzalez <marcos.plaza.gonzalez@gmail.com>
2018-12-11 08:50:44 +0100

    -graphical resources -vista

---
+++ src/vista/MainWindow.java
@@ -0,0 +1,14 @@
+package vista;
+
+import javax.swing.*;
+
+public class MainWindow {
+    private JPanel finestraPrincipal;
+    private JTextArea appTitle;
+    private JList cataleg;
+    private JList watchedList;
+    private JList watchNext;
+    private JList notStartedYet;
+
+
+}
