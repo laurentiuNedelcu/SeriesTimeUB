52dacbe3e3b91c9db4fa69876cd73fc87e645b90
marcosplazagonzalez <marcos.plaza.gonzalez@gmail.com>
2018-12-27 18:34:36 +0100

    Inclusion de nuevos frames Commit de prueba

--- src/vista/STUB3.java
+++ src/vista/STUB3.java
@@ -2,12 +2,14 @@ package vista;
 
 import controller.Controlador;
 
+import javax.swing.*;
+
 public class STUB3 {
     public static void main(String args[]) {
         Controlador contr = new Controlador();
         contr.registreUsuari("userTest","Testeig","1234","Bcn","Test","12345678","03/02","Bcn C/Proba nº1",false);
-        contr.logInUsuari("Test","12345678");
-        MainWindow vista  = new MainWindow(contr);
+        JOptionPane.showMessageDialog(null,"Hem registrat un únic usuari amb nom d'usuari: Test, i contrasenya: 12345678, per a no haver de fer la interfície gráfica de 'Registrar usuari'!","DADA IMPORTANT!", JOptionPane.INFORMATION_MESSAGE);
+        STUB_LogInSignIn vista  = new STUB_LogInSignIn(contr);
         vista.setVisible(true);
 
     }
